19919e659e5d4759d5005d6bf68e3e73cbc68688
AndyHWChung <26929776+AndyHWChung@users.noreply.github.com>
2023-09-25 15:21:31 +0100

    formatting

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,12 +9,16 @@ object AppDependencies {
   private val hmrcMongoVersion = "1.3.0"
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % hmrcMongoVersion
+  // format: OFF
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % hmrcMongoVersion
+  // format: ON
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootstrapVersion % "test, it",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28" % hmrcMongoVersion % Test,
+  // format: OFF
+    "uk.gov.hmrc"       %% "bootstrap-test-play-28"  % bootstrapVersion % "test, it",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28" % hmrcMongoVersion % Test
+  // format: ON
   )
 }
