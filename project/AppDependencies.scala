f158b85c85835baefd73552a55792f9b9aca2150
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2023-09-26 09:37:36 +0100

    [AC][OPS-11028] set up developing tools (#1)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,13 +8,21 @@ object AppDependencies {
   private val bootstrapVersion = "7.22.0"
   private val hmrcMongoVersion = "1.3.0"
 
-  val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % hmrcMongoVersion
+  val compile: Seq[ModuleID] = Seq(
+  // format: OFF
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % hmrcMongoVersion
+  // format: ON
   )
 
-  val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % bootstrapVersion            % "test, it",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % hmrcMongoVersion            % Test,
-  )
+  val test: Seq[ModuleID] = Seq(
+  // format: OFF
+    "uk.gov.hmrc"       %% "bootstrap-test-play-28"  % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28" % hmrcMongoVersion
+  // format: ON
+  ).map(_ % Test)
+
+  // only add additional dependencies here - it test inherit test dependencies above already
+  val itDependencies: Seq[ModuleID] = Seq()
+
 }
