5f74ce34bc5e73e999ea59f1eb1470527c07eb65
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2025-03-26 09:11:37 +0000

    [AC][OPS-13453] dependency upgrades; allowing for java 21 (#11)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.6.0"
-  private val hmrcMongoVersion = "2.3.0"
+  private val bootstrapVersion = "9.11.0"
+  private val hmrcMongoVersion = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
   // format: OFF
